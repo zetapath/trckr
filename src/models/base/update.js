5701537233a438d1344d3cedbedcef257ca7c031
@soyjavi <javi.jimenez.villar@gmail.com>
2016-08-12 18:28:54 +0700

    When call model.update() ... refresh automatic field updatedAt

--- src/models/base/update.js
+++ src/models/base/update.js
@@ -3,15 +3,18 @@ import uuid from 'uuid'
 export default (state) => ({
 
   update(props) {
+    props.data.updatedAt = new Date()
     let item = state.db
       .get(state.key)
       .find(props.query)
       .assign(props.data)
       .value()
 
-    if ((!item || !item.id ) && props.data && props.upsert) {
+    if ((!item || !item.id) && props.data && props.upsert) {
+      console.log('💩', item)
       props.data.id = uuid()
       props.data.createdAt = new Date()
+      delete props.data.updatedAt
       item = state.db
         .get(state.key)
         .push(props.data)
