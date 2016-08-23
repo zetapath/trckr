cb7ce658668c007b6bea541581abb83aebfa03bd
@soyjavi <javi.jimenez.villar@gmail.com>
2016-08-23 19:21:20 +0700

    Don't show add order when user doesn't have session

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -4,6 +4,7 @@ import Tooltip from 'react-toolbox/lib/tooltip';
 import AppBar from './components/AppBar';
 import DialogNewOrder from './components/DialogNewOrder';
 import style from './app.css';
+import session from '../../modules/session';
 
 const TooltipButton = Tooltip(Button);
 
@@ -19,22 +20,25 @@ export default class Orders extends React.Component {
   }
 
   // -- Render
+  renderButton = () => {
+    return (
+      <TooltipButton
+        icon='add'
+        tooltip='New package'
+        className={style.newOrder}
+        floating
+        accent
+        onClick={this.handleToggle}
+      />
+    );
+  }
+
   render() {
     return (
       <section className={style.root}>
-        <AppBar />
-        <TooltipButton
-          icon='add'
-          tooltip='New package'
-          className={style.newOrder}
-          floating
-          accent
-          onClick={this.handleToggle}
-        />
-        <DialogNewOrder
-          active={this.state.dialog}
-          onClose={this.handleToggle}
-        />
+        <AppBar session={session} />
+        { session ? this.renderButton() : null }
+        <DialogNewOrder active={this.state.dialog} onClose={this.handleToggle} />
         {this.props.children}
       </section>
     );
--- app/components/app/components/AppBar.jsx
+++ app/components/app/components/AppBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router';
 import AppBar from 'react-toolbox/lib/app_bar';
 import Navigation from 'react-toolbox/lib/navigation';
-import session from '../../../modules/session';
 import style from './AppBar.css';
 
 export default (props) => {
@@ -10,9 +9,9 @@ export default (props) => {
     <AppBar fixed flat className={style.root}>
       <Link to='/' className={style.brand}>Trckr</Link>
       <Navigation type='horizontal' className={style.navigation}>
-        { !session ? <Link to='/signin' activeClassName={style.active}>Sign In</Link> : null }
-        { session ? <Link to='/orders' activeClassName={style.active}>Orders</Link> : null }
-        { session ? <Link to='/profile' activeClassName={style.active}>profile</Link> : null }
+        { !props.session ? <Link to='/signin' activeClassName={style.active}>Sign In</Link> : null }
+        { props.session ? <Link to='/orders' activeClassName={style.active}>Orders</Link> : null }
+        { props.session ? <Link to='/profile' activeClassName={style.active}>profile</Link> : null }
       </Navigation>
     </AppBar>
   );
